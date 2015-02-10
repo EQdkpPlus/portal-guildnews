e1a618a2f475991f50821b9b0950222acd324658
GodMod <GodMod@users.noreply.github.com>
2015-02-10 09:48:16 +0100

    Update guildnews_portal.class.php

--- guildnews_portal.class.php
+++ guildnews_portal.class.php
@@ -28,7 +28,7 @@ class guildnews_portal extends portal_generic {
 	protected static $path		= 'guildnews';
 	protected static $data		= array(
 		'name'			=> 'Guildnews',
-		'version'		=> '0.2.0',
+		'version'		=> '0.2.1',
 		'author'		=> 'GodMod',
 		'contact'		=> EQDKP_PROJECT_URL,
 		'description'	=> 'Show Guildnews from WoW Armory',
